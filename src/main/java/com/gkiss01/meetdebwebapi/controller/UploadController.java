4b45cb7be1153bb50ab18244bf5373451b1fd1b9
gkiss01 <gergokiss05@gmail.com>
2020-03-06 18:30:26 +0100

    Kiszolgált képek élettartamának beállítása

--- src/main/java/com/gkiss01/meetdebwebapi/controller/UploadController.java
+++ src/main/java/com/gkiss01/meetdebwebapi/controller/UploadController.java
@@ -4,11 +4,12 @@ import com.gkiss01.meetdebwebapi.model.GenericResponse;
 import com.gkiss01.meetdebwebapi.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping(path = "images/{eventId}")
@@ -31,6 +32,9 @@ public class UploadController {
     public ResponseEntity<Resource> readFile(@PathVariable Long eventId) {
         Resource resource = fileService.loadFile(eventId);
 
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(resource);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS));
+        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
     }
 }
