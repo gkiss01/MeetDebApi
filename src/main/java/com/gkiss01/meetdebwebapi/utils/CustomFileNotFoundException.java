fdf2bdd60c2716874b186f427583d1029b415837
gkiss01 <gergokiss05@gmail.com>
2020-05-30 21:14:44 +0200

    További kód tisztítás

--- src/main/java/com/gkiss01/meetdebwebapi/utils/CustomFileNotFoundException.java
+++ src/main/java/com/gkiss01/meetdebwebapi/utils/CustomFileNotFoundException.java
@@ -3,13 +3,10 @@ package com.gkiss01.meetdebwebapi.utils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Data
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CustomFileNotFoundException extends RuntimeException {
     private ErrorCodes errorCode;
 }
